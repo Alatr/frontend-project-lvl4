6c50454abb92450c130ee48f6cd50126f76803fe
Dmytro Lukianov <dimalukianovweb@gmail.com>
2021-07-03 15:42:51 +0300

    #2

--- src/init.jsx
+++ src/init.jsx
@@ -8,8 +8,8 @@ import { io } from 'socket.io-client';
 import { setLocale } from 'yup';
 import { useTranslation, I18nextProvider } from 'react-i18next';
 
-import configureStore from '@lib/configure-store.js';
-import i18n from '@lib/i18n.js';
+import configureStore from './lib/configure-store.js';
+import i18n from './lib/i18n.js';
 import { useAuth } from './hooks/index.js';
 import { addChannel, removeChannel, renameChannel } from './slices/channels.js';
 import { addMessage } from './slices/messages.js';
